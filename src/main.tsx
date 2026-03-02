f9b5c10b2409eaab7a61e1a866d1644e5521144c
John Developer <john@example.com>
2026-03-02 14:36:35 +0300

    new changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App1 from './App1.tsx'
 import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <App1 />
     </ThemeProvider>
   </StrictMode>,
 )
